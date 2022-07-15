29f214bc7ba754204ef2b9362899bfb86152bf6f
palebluedot13 <13blackjack.cs@gmail.com>
2022-07-15 00:44:59 +0000

    Auto Commit

--- app/main.py
+++ app/main.py
@@ -1,11 +1,25 @@
 # import requirements needed
 from flask import Flask, render_template
 from utils import get_base_url
+import joblib
+import sys
+sys.modules['sklearn.externals.joblib'] = joblib
+from sklearn.externals.joblib import dump, load
+import numpy as np
+import pickle
 
 # setup the webserver
 # port may need to be changed if there are multiple flask servers running on same server
 port = 12396
 base_url = get_base_url(port)
+
+def numbers_to_genre(argument):
+    switcher = {0: 'Electronic', 1: 'Anime', 2: 'Jazz', 3: 'Alternative', 4: 'Country', 5: 'Rap', 6: 'Blues', 7: 'Rock', 8: 'Classical', 9: 'Hip-Hop'}
+    return switcher.get(argument, "nothing")
+
+sc_load = load('std_scaler.bin')
+pickled_model = pickle.load(open('model.pkl', 'rb'))
+
 
 # if the base url is not empty, then the server is running in development, and we need to specify the static folder so that the static files are served
 if base_url == '/':
@@ -17,6 +31,20 @@ else:
 @app.route(f'{base_url}')
 def home():
     return render_template('website-code.html')
+
+
+
+@app.route(f"{base_url}", methods=['GET', 'POST'])
+def main():
+    if request.method == 'GET':
+        return (flask.render_template('index.html', prediction_text = ""))
+    if request.method == 'POST':
+        input_features = [float(item) for item in request.form.values()]
+        input_array = np.array(input_list)
+        scaled_input = sc_load.transform(input_array.reshape(1,-1))
+        genre_prediction = numbers_to_genre(pickled_model.predict(scaled_input)[0])
+        return render_template('index.html', prediction_text=genre_prediction)
+
 
 # define additional routes here
 # for example:
